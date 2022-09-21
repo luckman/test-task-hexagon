28975ffbc2a2c8eb89dfcb5a946de6dc66b2bbe9
Konstantin Porozov <porozov.kostya@gmail.com>
2022-09-21 12:38:52 +0200

    Initial commit

---
+++ src/test/java/com/luckman/hexagon/testtask/AppTest.java
@@ -0,0 +1,15 @@
+package com.luckman.hexagon.testtask;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("Sample test");
+        assertTrue(true);
+    }
+}
